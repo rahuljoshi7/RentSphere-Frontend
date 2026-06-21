d8cef8a621a31a95a0e8c819389c57984cf2ef66
Rahul Joshi <rahulmjoshi0007@gmail.com>
2026-06-21 22:11:08 +0530

    Update agreementService.js

--- src/services/agreementService.js
+++ src/services/agreementService.js
@@ -1,12 +1,36 @@
 import api from './axios'
 
 export const agreementService = {
-  create:         (data)       => api.post('/agreements', data),
-  getById:        (id)         => api.get(`/agreements/${id}`),
-  getMine:        (params)     => api.get('/agreements/my', { params }),
-  getByProperty:  (id, params) => api.get(`/agreements/property/${id}`, { params }),
-  getByTenant:    (id, params) => api.get(`/agreements/tenant/${id}`, { params }),
-  uploadDocument: (id, fd)     => api.post(`/agreements/${id}/document`, fd, { headers: { 'Content-Type': 'multipart/form-data' } }),
-  terminate:      (id)         => api.patch(`/agreements/${id}/terminate`),
-  renew:          (id, data)   => api.post(`/agreements/${id}/renew`, data)
+  create: (data) =>
+    api.post('/agreements', data),
+
+  getById: (id) =>
+    api.get(`/agreements/${id}`),
+
+  // Tenant Agreements
+  getMyAgreements: (params) =>
+    api.get('/agreements/my', { params }),
+
+  // Owner/Admin Agreements
+  getOwnerAgreements: (params) =>
+    api.get('/agreements/owner/my', { params }),
+
+  getByProperty: (id, params) =>
+    api.get(`/agreements/property/${id}`, { params }),
+
+  getByTenant: (id, params) =>
+    api.get(`/agreements/tenant/${id}`, { params }),
+
+  uploadDocument: (id, formData) =>
+    api.post(`/agreements/${id}/document`, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    }),
+
+  terminate: (id) =>
+    api.patch(`/agreements/${id}/terminate`),
+
+  renew: (id, data) =>
+    api.post(`/agreements/${id}/renew`, data)
 }
